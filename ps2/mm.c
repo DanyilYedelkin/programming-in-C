3168f54bb04e0cd466318bc09366e9e3d13d1470
Danyil Yedelkin <danyil.yedelkin@student.tuke.sk>
2021-03-20 20:46:41 +0000

    Update 1: mm.c

--- ps2/mm.c
+++ ps2/mm.c
@@ -18,26 +18,24 @@ int main(){
 int mighty_method(char *number){
     unsigned long long sum = 0;
     char num = number[0];
-    for(long long i=0; i<strlen(number); i++){
+    for(long long i = 0; i < strlen(number); i++){
         num = number[i];
         sum += num - '0';
-        if(number[i+1]=='\0'){
-            break;
-        }
+        if(number[i + 1] == '\0') break;
     }
-    if(sum>0 && sum<10){
-        return sum;
-    } else{
+    if((sum > 0) && (sum < 10)) return sum;
+    else{
         char *change = calloc(100000000, sizeof(char));
         char new_num = 'a';
         long a = 0;
-        for(int i=0; i<10 && sum!=0; i++, sum/=10, a++){
-            new_num = (sum%10) + '0';
+        for(long i = 0; (i < 10) && (sum != 0); i++, sum /= 10, a++){
+            new_num = (sum % 10) + '0';
             change[i] = new_num;
+            if((i + 1) == 10) change[a+1] = '\0';
         }
-        change[a] = '\0';
         int new_sum = mighty_method(change);
-        free(change);
+        if(change != NULL) free(change);
+        else return 0;
         return new_sum;
     }
 }
