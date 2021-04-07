852b063968cbe08f70ba487c820ae73fb4d88c11
Danyil Yedelkin <danyil.yedelkin@student.tuke.sk>
2021-04-07 15:11:05 +0000

    Update 1: main.c

--- ps3/main.c
+++ ps3/main.c
@@ -1,37 +1,69 @@
 #include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "k.h"
 #include "ui.h"
 #include "hof.h"
 
+
 int main()
 {
     struct player *list = malloc(sizeof(struct player) * 10);
-    int hof_size = load(list);
-
     struct player *player1 = malloc(sizeof(player1));
-    player1->score = 300;
-    char *name = "John3";
+    player1->score = 0;
+    char *name = "John";
     strcpy(player1->name, name);
+    int size_fp = load(list);
 
-    add_player(list, &hof_size, *player1);
-    save(list, hof_size);
+    for (int i = 0; i < size_fp; i++){
+        printf("%s%s%d\n", list[i].name, DELIMITER, list[i].score);
+    }
 
-    /*struct player list[10];
-
-    struct player *list = malloc(sizeof(struct player) * 10);
-    int size = load(list);
-    struct player player = {
-        .name = "John",
-        .score = 100
+    struct game game = {
+            .board = {
+                    {'A', ' ', ' ', ' '},
+                    {' ', ' ', ' ', ' '},
+                    {' ', ' ', ' ', ' '},
+                    {'A', ' ', ' ', 'A'}
+            },
+            .score = 0
     };
 
-    add_player(list, &size, player);
-    save(list, size);*/
-
-    for(int i = 0; i < hof_size; i++){
-        printf("%s\t%d\n", list[i].name, list[i].score);
+    render(game);
+    while(1){
+        int step = getchar();
+        switch(step){
+            case 'w':{
+                if (update(&game, -1, 0)) add_random_tile(&game);
+                render(game);
+                break;
+            }
+            case 'd':{
+                if (update(&game, 0, 1)) add_random_tile(&game);
+                render(game);
+                break;
+            }
+            case 's':{
+                if (update(&game, 1, 0)) add_random_tile(&game);
+                render(game);
+                break;
+            }
+            case 'a':{
+                if (update(&game, 0, -1)) add_random_tile(&game);
+                render(game);
+                break;
+            }
+            case 'q':{
+                add_player(list, &size_fp, *player1);
+                save(list, size_fp);
+                free(list);
+                free(player1);
+                return 0;
+            }
+            default:
+                break;
+        }
     }
 
     return 0;
