8fd9058e20266ca78ad8e6106737b77d04c65410
Danyil Yedelkin <danyil.yedelkin@student.tuke.sk>
2021-04-23 16:27:46 +0000

    Update 1: main.c

--- ps4/main.c
+++ ps4/main.c
@@ -4,6 +4,25 @@
 #include "transformations.h"
 #include <string.h>
 
+/*
+         _nnnn_                      
+        dGGGGMMb     ,"""""""""""""".
+       @p~qp~~qMb    | Linux Rules! |
+       M|@||@) M|   _;..............'
+       @,----.JM| -'
+      JS^\__/  qKL
+     dZP        qKRb
+    dZP          qKKb
+   fZP            SMMb
+   HZM            MMMM
+   FqM            MMMM
+ __| ".        |\dS"qML
+ |    `.       | `' \Zq
+_)      \.___.,|     .'
+\____   )MMMMMM|   .'
+     `-'       `--' hjm
+*/
+
 int main(){
     FILE* input = fopen("assets/lenna.bmp", "rb");
     struct bmp_image* image_bmp = read_bmp(input);
