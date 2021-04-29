b088d62aed59776495b53b57d088db5df9a9d844
Danyil Yedelkin <danyil.yedelkin@student.tuke.sk>
2021-04-29 11:57:35 +0000

    Update 7: My mother told me Someday I would buy Galleys with good oars Sail to distant shores

--- ps4/bmp.c
+++ ps4/bmp.c
@@ -28,23 +28,23 @@ struct bmp_header* read_bmp_header(FILE* stream){
     //https://web.cs.ucdavis.edu/~amenta/s12/readBMP.cpp
     //https://elcharolin.wordpress.com/2018/11/28/read-and-write-bmp-files-in-c-c/
     if(stream == NULL) return NULL;
-    fseek(stream, 0, SEEK_END);
+    fseek(stream, 0, 2);
     //http://www.c-cpp.ru/content/ftell
-    int length_file = ftell(stream);
+    uint32_t length_file = ftell(stream);
     if(length_file == -1L) return NULL;
 
-    fseek(stream, 0, SEEK_SET);
+    fseek(stream, 0, 0);
     char *string_of_steam = calloc(length_file, sizeof(char));
     fread(string_of_steam, length_file, 1, stream);
     char *long_of_string = calloc(strlen(string_of_steam), sizeof(char));
 
-    for(int i = 0, number = 0; i < length_file; i++){
+    for(uint32_t i = 0, number = 0; i < length_file; i++){
         if(string_of_steam[i] != '\0'){
             number++;
             long_of_string[number] = string_of_steam[i];
         }
         if(string_of_steam[i+1] == 77 && string_of_steam[i] == 66){
-            fseek(stream, i, SEEK_SET);
+            fseek(stream, i, 0);
             break;
         } else if(string_of_steam[i+1] == '\0'){
             free(string_of_steam);
@@ -68,15 +68,19 @@ struct bmp_header* read_bmp_header(FILE* stream){
 struct pixel* read_data(FILE* stream, const struct bmp_header* header){
     if((stream == NULL) || (header == NULL)) return NULL;
 
-    int size = header->height * header->width;
+    //creating new variables for simple codding 
+    uint32_t size = header->height * header->width;
+    uint32_t width = header->width;
+    uint32_t height = header->height;
     struct pixel* data = calloc(size, sizeof(struct pixel));
-    fseek(stream, header->offset, SEEK_SET);
+    fseek(stream, header->offset, 0);
 
-    for(int i = 0; i < header->height; i++){
-        for(int j = 0; j < header->width; j++){
-            fread(&data[i * header->width + j], sizeof(struct pixel), 1, stream);
+    //function for reading data memory
+    for(uint32_t i = 0; i < height; i++){
+        for(uint32_t j = 0; j < width; j++){
+            fread(&data[i * width + j], sizeof(struct pixel), 1, stream);
         }
-        if((header->width % 4) != 0) fseek(stream, header->width, SEEK_CUR);
+        if((width % 4) != 0) fseek(stream, width, 1);
     }
 
     return data;
@@ -85,6 +89,7 @@ struct pixel* read_data(FILE* stream, const struct bmp_header* header){
 struct bmp_image* read_bmp(FILE* stream){
     if(stream == NULL) return NULL;
 
+    //creating and checking a header bmp (for elements)
     struct bmp_header* header = read_bmp_header(stream);
     if(header == NULL){
         fprintf(stderr, "Error: This is not a BMP file.\n");
@@ -92,6 +97,7 @@ struct bmp_image* read_bmp(FILE* stream){
         return NULL;
     }
 
+    //creating and checking a data bmp (for elements)
     struct pixel* data = read_data(stream, header);
     if(data == NULL){
         fprintf(stderr, "Error: Corrupted BMP file.\n");
@@ -99,7 +105,8 @@ struct bmp_image* read_bmp(FILE* stream){
         free(data);
         return NULL;
     }
-    
+
+    //creating a new bmp image, and fill it with data & header
     struct bmp_image* image = malloc(sizeof(struct bmp_image));
     if(header != NULL && data != NULL){
         image->data = data;
@@ -110,14 +117,16 @@ struct bmp_image* read_bmp(FILE* stream){
 }
 
 bool write_bmp(FILE* stream, const struct bmp_image* image){
-    if((stream == NULL) || (stream == NULL)) return false;
+    if((stream == NULL) || (image == NULL) || (image->data == NULL) || (image->header == NULL)) return false;
 
+    //creating new variables for simple codding 
     uint32_t height = image->header->height;
     uint32_t width = image->header->width;
 
+    //function for writing data
     fwrite(image->header, sizeof(struct bmp_header), 1, stream);
-    for(int i = 0; i < height; i++){
-        for(int j = 0; j < width; j++){
+    for(uint32_t i = 0; i < height; i++){
+        for(uint32_t j = 0; j < width; j++){
             fwrite(&image->data[(i * width) + j], sizeof(struct pixel), 1, stream);
         }
         fwrite(&PADDING_CHAR, sizeof(unsigned char), ((width) % 4), stream);
@@ -133,4 +142,3 @@ void free_bmp_image(struct bmp_image* image){
         free(image);
     }
 }
-
