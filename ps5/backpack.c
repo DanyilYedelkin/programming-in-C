d0cc8f514581bf2b52693422aadc47bb65069b74
Danyil Yedelkin <danyil.yedelkin@student.tuke.sk>
2021-05-08 23:50:49 +0000

    Update 1: backpack.c

--- ps5/backpack.c
+++ ps5/backpack.c
@@ -5,6 +5,7 @@
 #include "backpack.h"
 
 struct backpack* create_backpack(const int capacity){
+    //first changed: changed malloc -> calloc
     //struct backpack* new_backpack = malloc(sizeof(struct backpack));
     struct backpack* new_backpack = calloc(1, sizeof(struct backpack));
     new_backpack->capacity = capacity;
@@ -25,10 +26,27 @@ bool add_item_to_backpack(struct backpack* backpack, struct item* item){
     if((item == NULL) || (backpack == NULL)) return false;
 
     if(backpack->capacity > backpack->size){
-        backpack->items = create_container(backpack->items, ITEM, item);
+        //first changed: remove old method, add new
+        /*backpack->items = create_container(backpack->items, ITEM, item);
         backpack->size++;
-        return true;
+        return true;*/
+        struct container* items = backpack->items;
+        if(items != NULL){
+            while(items->next != NULL){
+                if(backpack->capacity < backpack->size){
+                    return false;
+                }
+                items = items->next;
+            }
+            backpack->items = create_container(backpack->items, ITEM, item);
+            backpack->size++;
+        } else if(items == NULL){
+            backpack->items = create_container(backpack->items, ITEM, item);
+            backpack->size++;
+        }
     } else return false;
+
+    return true;
 }
 
 struct item* get_item_from_backpack(const struct backpack* backpack, char* name){
@@ -40,6 +58,27 @@ struct item* get_item_from_backpack(const struct backpack* backpack, char* name)
 void delete_item_from_backpack(struct backpack* backpack, struct item* item){
     if((backpack == NULL) || (item == NULL)) return;
 
-    backpack->items = remove_container(backpack->items, item);
+    //first changed: remove old method, add new
+    /*backpack->items = remove_container(backpack->items, item);
+    backpack->size--;*/
+
+    struct container* items = backpack->items;
+    if(items->item == item){
+        struct container* delete_first = backpack->items;
+        backpack->items = backpack->items->next;
+        free(delete_first);
+        backpack->size--;
+
+        return;
+    }
+    while((items->next->item != item) && (items->next != NULL)){
+        items = items->next;
+    }
+    if((items->item != item) || (items->next == NULL)){
+        return;
+    }
+    struct container* delete_first = items->next;
+    items->next = items->next->next;
+    free(delete_first);
     backpack->size--;
 }
