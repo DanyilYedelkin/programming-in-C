55efc0fdfdf318a1b4d24f5797f1b41345897a6d
Danyil Yedelkin <danyil.yedelkin@student.tuke.sk>
2021-05-12 15:44:42 +0000

    Update room.c

--- ps5/room.c
+++ ps5/room.c
@@ -1,16 +1,37 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 #include <ctype.h>
+#include <string.h>
 #include "room.h"
+
+/*
+         _nnnn_                      
+        dGGGGMMb     ,"""""""""""""".
+       @p~qp~~qMb    | Linux Rules! |
+       M|@||@) M|   _;..............'
+       @,----.JM| -'
+      JS^\__/  qKL
+     dZP        qKRb
+    dZP          qKKb
+   fZP            SMMb
+   HZM            MMMM
+   FqM            MMMM
+ __| ".        |\dS"qML
+ |    `.       | `' \Zq
+_)      \.___.,|     .'
+\____   )MMMMMM|   .'
+     `-'       `--' hjm
+
+*/
+
 
 void creating_room(struct room* new_room, char* name, char* description);
 
 struct room* create_room(char *name, char *description){
     if((name == NULL) || (description == NULL) || (strlen(name) == 0) || (strlen(description) == 0)) return NULL;
 
-    struct room* new_room = malloc(sizeof(struct room));
-    //struct room* new_room = calloc(1, sizeof(struct room));
+    //struct room* new_room = malloc(sizeof(struct room));
+    struct room* new_room = calloc(1, sizeof(struct room));
     new_room->name = calloc(strlen(name)+1, sizeof(struct room));
     new_room->description = calloc(strlen(description)+1, sizeof(struct room));
     creating_room(new_room, name, description);
@@ -21,12 +42,14 @@ struct room* create_room(char *name, char *description){
 void creating_room(struct room* new_room, char* name, char* description){
     strcpy(new_room->name, name);
     strcpy(new_room->description, description);
+
     new_room->items = NULL;
     new_room->south = NULL;
     new_room->north = NULL;
     new_room->east = NULL;
     new_room->west = NULL;
 }
+
 
 void set_exits_from_room(struct room *room, struct room *north, struct room *south, struct room *east, struct room *west){
     if(room == NULL) return;
@@ -63,12 +86,13 @@ void show_room(const struct room* room){
     } else printf("Nothing\n");
 }
 
+
 struct room* destroy_room(struct room* room){
     if(room == NULL) return NULL;
 
     destroy_containers(room->items);
-    free(room->name);
-    free(room->description);
+    if(room->name != NULL) free(room->name);
+    if(room->description != NULL) free(room->description);
     free(room);
 
     return NULL;
@@ -77,7 +101,7 @@ struct room* destroy_room(struct room* room){
 void add_item_to_room(struct room* room, struct item* item){
     if((room == NULL) || (item == NULL)) return;
 
-    room->items = create_container(room->items, ROOM, item);
+    room->items = create_container(room->items, ITEM, item);
 }
 
 struct item* get_item_from_room(const struct room* room, const char* name){
@@ -86,11 +110,16 @@ struct item* get_item_from_room(const struct room* room, const char* name){
     return get_from_container_by_name(room->items, name);
 }
 
-
-void delete_item_from_room(struct room* room, struct item* item){
+void delete_item_from_room(struct room* room, struct item* item) {
     if((item == NULL) || (room == NULL)) return;
 
+    //new changing
     struct container* container = room->items;
+    if(container->item == item){
+        room->items = container->next;
+        free(container);
+        return;
+    }
     while(container->next != NULL){
         if(container->next->item == item){
             container->next = container->next->next;
@@ -99,35 +128,4 @@ void delete_item_from_room(struct room* room, struct item* item){
         }
         container = container->next;
     }
-    if(container->item == item){
-        room->items = container->next;
-        free(container);
-        return;
-    }
-
-    //room->items = remove_container(room->items, item);
 }
-
-/*int main(){
-    // create rooms first
-    struct room* home = create_room("start", "Nachadzas sa v chyzi svarneho suhaja. Na vychode sa nachadzaju dvere veduce z chyze von.");
-    struct room* garden = create_room("garden", "Stojis pred chyzou a rozoznavas zahradku, ktora je znacne neudrziavana. este ze husty lesik na severe v porovnani so zahradkou nicim nevynika.");
-
-    // set exits
-    set_exits_from_room(home, NULL, NULL, garden, NULL);
-    set_exits_from_room(garden, NULL, NULL, NULL, home);
-
-    // show room
-    show_room(home);
-    
-    //Nachadzas sa v chyzi svarneho suhaja. Na vychode sa nachadzaju dvere veduce z chyze von.
-    //Mozne vychody z miesnosti:
-        //vychod
-    
-
-    // destroy rooms
-    home = destroy_room(home);
-    garden = destroy_room(garden);
-
-    return 0;
-}*/
