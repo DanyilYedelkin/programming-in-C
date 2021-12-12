1568fb98edee7114876e0adbcd978ceb2f02d1ec
Danyil Yedelkin <79505924+DanyilYedelkin@users.noreply.github.com>
2021-12-12 16:52:49 +0100

    Update command.c

--- ps5/command.c
+++ ps5/command.c
@@ -1,34 +1,11 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include <string.h>
 #include "command.h"
-
-/*
-         _nnnn_                      
-        dGGGGMMb     ,"""""""""""""".
-       @p~qp~~qMb    | Linux Rules! |
-       M|@||@) M|   _;..............'
-       @,----.JM| -'
-      JS^\__/  qKL
-     dZP        qKRb
-    dZP          qKKb
-   fZP            SMMb
-   HZM            MMMM
-   FqM            MMMM
- __| ".        |\dS"qML
- |    `.       | `' \Zq
-_)      \.___.,|     .'
-\____   )MMMMMM|   .'
-     `-'       `--' hjm
-
-*/
 
 
 struct command* create_command(char* name, char* description, char* pattern, size_t nmatch){
     if((name == NULL) || (description == NULL) || (strlen(name) <= 0) || (strlen(description) <= 0)) return NULL;
 
-    //first changed: change malloc -> calloc
-    //struct command* new_command = malloc(sizeof(struct command));
     struct command* new_command = calloc(1, sizeof(struct command));
 
     new_command->name = malloc(strlen(name)+1);
@@ -37,13 +14,6 @@ struct command* create_command(char* name, char* description, char* pattern, siz
     strcpy(new_command->description, description);
 
     new_command->nmatch = nmatch;
-
-    //https://man7.org/linux/man-pages/man3/regex.3.html
-    //first changed: delete function and create a new
-    /*if(pattern != NULL){
-        regcomp(&(new_command->preg), pattern, REG_ICASE | REG_EXTENDED);
-    }*/
-    //https://man7.org/linux/man-pages/man3/regex.3.html
     if(pattern != NULL){
         if(regcomp(&(new_command->preg), pattern, REG_ICASE | REG_EXTENDED) != 0){
             free(new_command);
